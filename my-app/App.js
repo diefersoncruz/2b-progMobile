03259e825907a8c7706b35abf8cf159bf27fd03d
Dieferson Cruz <dieferson.cruz@outlook.com>
2023-10-16 08:46:24 -0300

    Ajuste das APIs

--- my-app/App.js
+++ my-app/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { StyleSheet, TextInput, View, Image, Button } from 'react-native';
 import { useState } from 'react';
 
 export default function App() {
@@ -7,14 +7,14 @@ export default function App() {
   const [textoUsuario, setTextoUsuario] = useState('');
 
   function pegarImagemPokemon() {
-    fetch("https://pokeapi.co/api/v2/pokemon/mewtwo")
+    fetch("https://pokeapi.co/api/v2/pokemon/" + textoUsuario)
       .then((response) => { return response.json() })
       .then(json => { setPokemon(json.sprites.front_default) })
   }
 
   return (
     <View style={styles.container}>
-      <Image source={{ uri: pokemon }} style={{ width: 100, height: 100 }} />
+      <Image source={{ uri: pokemon }} style={styles.imagem} />
       <Button title='Pesquisar' onPress={pegarImagemPokemon} />
       <TextInput value={textoUsuario} onChangeText={setTextoUsuario} style={styles.input} />
       <StatusBar style="auto" />
@@ -30,7 +30,15 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   input: {
+    marginTop: 5,
     backgroundColor: 'gray',
-    font
+    fontSize: 16,
+    color: 'white',
+    fontSize: 26,
+    fontWeight: 10
+  },
+  imagem: {
+    width: 100,
+    height: 100
   }
 });
